f561601dfcd1e24c2cc10b6c827ffe7fd544aa3e
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-04-17 16:10:00 +0430

    when toggle on --> dark mod is on too

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+const body = document.querySelector("#body");
 const switchBtn = document.querySelector(".switchBtn");
 const container = document.querySelector(".container");
 const threadEnd = document.querySelector(".threadEnd");
 
-switchBtn.addEventListener("click", () => container.classList.toggle("on"));
+switchBtn.addEventListener("click", () => {container.classList.toggle("on");
+	body.classList.toggle('off');
+});
 
 threadEnd.addEventListener('click', () => {
 	if(switchBtn.checked) {
 		switchBtn.checked = false;
 		container.classList.toggle('on');
+		body.classList.toggle('off');
 	} else {
 		switchBtn.checked = true;
 		container.classList.toggle('on');
+		body.classList.toggle('off');
 	}
 })	
+
